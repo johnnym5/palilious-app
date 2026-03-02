3d5946c2882c7cd0072aa071dafb4a3571c0482a
Egbase John Mary <johnmaryegbase@gmail.com>
2026-03-02 10:50:08 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2214070311-ec1c7",
+  "appId": "1:451324532097:web:d9c9ab0982e05698500480",
+  "apiKey": "AIzaSyBS2ZAdTI4iZt2teYo8R5nezwpIGPMt-aY",
+  "authDomain": "studio-2214070311-ec1c7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "451324532097"
+};
