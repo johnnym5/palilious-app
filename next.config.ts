cf2cf1b946e344285e0c8ef5ac18391aab709a57
Egbase John Mary <johnmaryegbase@gmail.com>
2026-03-04 12:01:09 +0000

    npm run build
    > palilious@0.1.0 build
    > next build
    
    > Build error occu

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  distDir: 'public',
+  distDir: 'out',
   typescript: {
     ignoreBuildErrors: true,
   },
