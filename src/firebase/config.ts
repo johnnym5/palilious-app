57a113d86d40f3bcbdba6209f097637d4fab114d
Egbase John Mary <johnmaryegbase@gmail.com>
2026-03-04 13:55:44 +0000

    PLEASE ARE ALL MY AIKEYS AND SECETS HDDEN?

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyDzEUJ8HQ0SMLhuyBjXe8l7PKZL5i49III",
-    authDomain: "palilious.firebaseapp.com",
-    projectId: "palilious",
-    storageBucket: "palilious.firebasestorage.app",
-    messagingSenderId: "670593045621",
-    appId: "1:670593045621:web:0ab9bb120a1aeed9b5143c",
-    measurementId: "G-FGS78EHV3R"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
